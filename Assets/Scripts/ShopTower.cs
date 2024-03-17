1aa2e2ce5b4457d4780ed924e23f393f2466bbd7
scout2281337 <146185803+scout2281337@users.noreply.github.com>
2024-03-17 17:22:36 +0300

    0.0.2v2

---
+++ Assets/Scripts/ShopTower.cs
@@ -0,0 +1,15 @@
+using System;
+using UnityEngine;
+[Serializable]
+public class ShopTower {
+
+    public string name;
+    public int cost;
+    public GameObject prefab;
+
+    public ShopTower(string _name, int _cost, GameObject _prefab) { 
+        name = _name;
+        cost = _cost;
+        prefab = _prefab;
+    }
+}
