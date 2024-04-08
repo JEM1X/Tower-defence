8b2dcd827f093dfaa20d5dc00a4ed91daaef603e
scout2281337 <146185803+scout2281337@users.noreply.github.com>
2024-04-09 00:17:19 +0300

    Raid Shadow Legends

--- Assets/Scripts/Castle.cs
+++ Assets/Scripts/Castle.cs
@@ -6,15 +6,17 @@ public class Castle : MonoBehaviour
 {
     public int MaxHP;
     public int HP;
-
+    public HealthBar healthBar;
     void Start()
     {
         HP = MaxHP;
+        healthBar.SetMaxHealth(MaxHP);
     }
 
     public void TakeDamage(int damageAmount)
     {
         HP -= damageAmount;
+        healthBar.SetHealth(HP);
         if (HP <= 0)
         {
             Destroy(gameObject); // Уничтожаем замок, если его здоровье меньше или равно нулю
