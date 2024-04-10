236c9a9dcc08fe185edc322bd7b803663889e8b0
Антон Кучер <kapetrovi4@gmail.com>
2024-04-10 05:49:13 +0300

    BLYAD

--- Assets/Scripts/Castle.cs
+++ Assets/Scripts/Castle.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Castle : MonoBehaviour
 {
@@ -19,7 +20,7 @@ public class Castle : MonoBehaviour
         healthBar.SetHealth(HP);
         if (HP <= 0)
         {
-            Destroy(gameObject); // Уничтожаем замок, если его здоровье меньше или равно нулю
+            Destroy(gameObject);
         }
     }
 }
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,10 +5,53 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    private GameObject UIGold;
+    private GameObject healthBar;
+    private GameObject inventory;
+    private GameObject shop;
+    private GameObject backpack;
+
+    void Start()
+    {
+        GameObject canvasObject = GameObject.Find("Canvas");
+        if (canvasObject != null)
+        {
+            Transform canvasTransform = canvasObject.transform;
+            UIGold = canvasTransform.Find("UIGold")?.gameObject;
+            healthBar = canvasTransform.Find("Health bar")?.gameObject;
+            inventory = canvasTransform.Find("MainInventory")?.gameObject;
+            shop = canvasTransform.Find("ShopButton")?.gameObject;
+            backpack = canvasTransform.Find("BackInventoryButton")?.gameObject;
+        }
+
+        if (UIGold != null)
+            UIGold.SetActive(false);
+        if (healthBar != null)
+            healthBar.SetActive(false);
+        if (inventory != null)
+            inventory.SetActive(false);
+        if (shop != null)
+            shop.SetActive(false);
+        if (backpack != null)
+            backpack.SetActive(false);
+    }
+
     public void play()
     {
-        SceneManager.LoadScene(1);
+        if (UIGold != null)
+            UIGold.SetActive(true);
+        if (healthBar != null)
+            healthBar.SetActive(true);
+        if (inventory != null)
+            inventory.SetActive(true);
+        if (shop != null)
+            shop.SetActive(true);
+        if (backpack != null)
+            backpack.SetActive(true);
+
+        SceneManager.LoadScene("GameScene");
     }
+
     public void Quite()
     {
         Application.Quit();
