f050c8139ca7039eed65e71f8411fea148398ddd
scout2281337 <146185803+scout2281337@users.noreply.github.com>
2024-04-11 23:18:50 +0300

    DildoPenetratorGAykiller

--- Assets/Scripts/Castle.cs
+++ Assets/Scripts/Castle.cs
@@ -5,29 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class Castle : MonoBehaviour
 {
-    public int MaxHP;
-    public int HP;
-    public HealthBar healthBar;
-    private bool isCastleDestroyed = false;
+    public int MaxHP;  // Максимальное количество здоровья замка
+    public int HP;      // Текущее количество здоровья замка
+    public HealthBar healthBar;  // Ссылка на HealthBar для отображения здоровья
+    private bool isCastleDestroyed = false;  // Флаг, указывающий, уничтожен ли замок
+
     void Start()
-    {   
-        HP = MaxHP;
-        healthBar.SetMaxHealth(MaxHP);
+    {
+        HP = MaxHP;  // Устанавливаем текущее здоровье равным максимальному
+        healthBar.SetMaxHealth(MaxHP);  // Устанавливаем максимальное значение здоровья на healthBar
     }
 
     public void TakeDamage(int damageAmount)
-    {   
-        if(!isCastleDestroyed) {
-            HP -= damageAmount;
-            healthBar.SetHealth(HP);
-            if (HP <= 0)
+    {
+        if (!isCastleDestroyed)
+        {  // Проверяем, не был ли замок уже уничтожен
+            HP -= damageAmount;  // Уменьшаем количество здоровья на величину урона
+            healthBar.SetHealth(HP);  // Обновляем отображение здоровья на healthBar
+            if (HP <= 0)  // Если здоровье стало меньше или равно нулю
             {
-                isCastleDestroyed = true;
-                SceneManager.LoadScene(2, LoadSceneMode.Additive);
-            
+                isCastleDestroyed = true;  // Устанавливаем флаг, что замок уничтожен
+                SceneManager.LoadScene(2, LoadSceneMode.Additive);  // Загружаем сцену с индексом 2
             }
         }
-        
     }
-   
 }
+
--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -4,16 +4,17 @@ using UnityEngine;
 using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
-
-    public Slider slider;
+    public Slider slider;  // Ссылка на компонент Slider, отвечающий за отображение полосы здоровья
 
     public void SetMaxHealth(int health)
-    { 
-        slider.maxValue = health;
-        slider.value = health;
+    {
+        slider.maxValue = health;  // Устанавливаем максимальное значение полосы здоровья равным переданному значению
+        slider.value = health;  // Устанавливаем текущее значение полосы здоровья равным переданному значению
     }
+
     public void SetHealth(int health)
     {
-        slider.value = health;
+        slider.value = health;  // Устанавливаем текущее значение полосы здоровья равным переданному значению
     }
 }
+
